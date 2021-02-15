3a5854bf2b2c370215427d5de7f986253cf48b92
spencerwooo <spencer.woo@outlook.com>
2021-02-16 02:15:47 +0800

    feat: initialise repo with vite, tailwindcss, react

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
+  },
+  theme: {
+    extend: {
+      fontWeight: ['hover', 'focus'],
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: []
+};
