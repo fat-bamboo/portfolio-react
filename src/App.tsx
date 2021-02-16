84a181cad9bf8739671a1fc4533156f14fad0129
spencerwooo <spencer.woo@outlook.com>
2021-02-17 03:30:46 +0800

    feat: hand wave emoji and bg colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import { AcademicCapOutline } from "heroicons-react";
+import { AcademicCapOutline } from 'heroicons-react'
 
 import Home from './views/Home'
 import Projects from './views/Projects'
@@ -9,32 +9,43 @@ import Projects from './views/Projects'
 function App() {
   return (
     <Router>
-      <div className="container mx-auto">
+      <div className="container mx-auto px-6">
         <div className="mt-4 flex flex-row justify-between">
-          <div className="flex">
+          <div className="flex p-2 pl-0">
             <AcademicCapOutline className="mr-4" />
-            <Link to="/">Portfolio</Link>
+            <Link to="/">Home</Link>
           </div>
 
           <div className="flex">
-            <Link className="mr-2" to="/projects">
+            <Link className="text-blue-700 p-2 mr-2 rounded" to="/projects">
               Projects
             </Link>
-            <Link className="mr-2" to="/">About</Link>
-            <Link to="/">About</Link>
+            <Link className="text-blue-700 p-2 mr-2 rounded" to="/">
+              Social
+            </Link>
+            <Link to="/" className="text-blue-700 p-2 rounded">
+              GPG
+            </Link>
           </div>
 
-          <div className="">Get in touch</div>
+          <a
+            className="bg-pink-50 text-pink-700 p-2 rounded hover:bg-pink-100"
+            href="mailto:spencer.wushangbo@gmail.com"
+          >
+            Get in touch
+          </a>
         </div>
 
-        <Switch>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <div className="mt-16">
+          <Switch>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import avatar from '../assets/avatar.png'
+import wave from '../assets/wave.png'
 
 function Home() {
   return (
     <div>
-      <div className="mt-16 text-center">
-        <img className="mx-auto shadow-xl rounded-full" src={avatar} alt="avatar" />
-        <div className="mt-6 text-xl font-bold">Spencer Woo</div>
-        <div></div>
+      <div className="flex justify-between mt-6">
+        <div className="mt-6 text-2xl font-bold flex">
+          <img className="w-8 h-8 mr-2 wave" src={wave} alt="wave" />
+          <div>Spencer Woo</div>
+        </div>
+        <img className="w-20 h-20 shadow-xl rounded-full" src={avatar} alt="avatar" />
       </div>
+
       <p>Hello from tailwindcss</p>
     </div>
   )
