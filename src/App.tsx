c5d7948ea89a4d57fe2e0aee82b5b8b46ed71ecf
spencerwooo <spencer.woo@outlook.com>
2021-02-17 21:13:47 +0800

    feat: router for pages, full width footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,66 @@ import { AcademicCapOutline } from 'heroicons-react'
 
 import Home from './views/Home'
 import Projects from './views/Projects'
+import NotFound from './views/NotFound'
+import Social from './views/Social'
+import GPG from './views/GPG'
 
 function App() {
+  const currentYear: number = new Date().getFullYear()
+
   return (
     <Router>
-      <div className="container mx-auto px-6">
-        <div className="mt-4 flex flex-row justify-between">
-          <div className="flex p-2 pl-0">
-            <AcademicCapOutline className="mr-4" />
-            <Link to="/">Home</Link>
+      <div className="min-h-screen flex flex-col">
+        <div className="container mx-auto px-6 flex-grow">
+          <nav className="mt-4 flex flex-row justify-between">
+            <Link className="flex p-2 rounded hover:bg-gray-100" to="/">
+              <AcademicCapOutline className="mr-4" />
+              <span>Home</span>
+            </Link>
+
+            <div className="flex">
+              <Link className="text-blue-700 p-2 mr-2 rounded hover:bg-blue-50" to="/projects">
+                Projects
+              </Link>
+              <Link className="text-blue-700 p-2 mr-2 rounded hover:bg-blue-50" to="/social">
+                Social
+              </Link>
+              <Link className="text-blue-700 p-2 rounded hover:bg-blue-50" to="/gpg">
+                GPG
+              </Link>
+            </div>
+
+            <a
+              className="bg-pink-50 text-pink-700 p-2 rounded hover:bg-pink-100"
+              href="mailto:spencer.wushangbo@gmail.com"
+            >
+              Get in touch
+            </a>
+          </nav>
+
+          <div className="mt-16">
+            <Switch>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/social">
+                <Social />
+              </Route>
+              <Route path="/gpg">
+                <GPG />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
           </div>
-
-          <div className="flex">
-            <Link className="text-blue-700 p-2 mr-2 rounded" to="/projects">
-              Projects
-            </Link>
-            <Link className="text-blue-700 p-2 mr-2 rounded" to="/">
-              Social
-            </Link>
-            <Link to="/" className="text-blue-700 p-2 rounded">
-              GPG
-            </Link>
-          </div>
-
-          <a
-            className="bg-pink-50 text-pink-700 p-2 rounded hover:bg-pink-100"
-            href="mailto:spencer.wushangbo@gmail.com"
-          >
-            Get in touch
-          </a>
         </div>
-
-        <div className="mt-16">
-          <Switch>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
+        <footer className="w-full text-center bg-gray-800 text-white p-4">
+          <div className="container mx-auto">Spencer Woo © 2019-{currentYear}</div>
+        </footer>
       </div>
     </Router>
   )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,13 +4,13 @@ import wave from '../assets/wave.png'
 
 function Home() {
   return (
-    <div>
-      <div className="flex justify-between mt-6">
-        <div className="mt-6 text-2xl font-bold flex">
+    <div className="pl-2">
+      <div className="flex justify-between items-center mt-6">
+        <div className="text-2xl font-bold flex">
           <img className="w-8 h-8 mr-2 wave" src={wave} alt="wave" />
           <div>Spencer Woo</div>
         </div>
-        <img className="w-20 h-20 shadow-xl rounded-full" src={avatar} alt="avatar" />
+        <img className="w-18 h-18 shadow-lg rounded-full" src={avatar} alt="avatar" />
       </div>
 
       <p>Hello from tailwindcss</p>
