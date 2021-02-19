531e0ca286e14a932a6cc1fcd9bd353a60700f59
spencerwooo <spencer.woo@outlook.com>
2021-02-20 00:24:50 +0800

    load star and fork from github

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,45 +1,45 @@
-import substats from '../assets/substats.png'
-import onedriveCfIndex from '../assets/onedrive-cf-index.png'
-import dowww from '../assets/dowww.png'
 import bithesis from '../assets/bithesis.png'
+import dowww from '../assets/dowww.png'
+import onedriveCfIndex from '../assets/onedrive-cf-index.png'
+import substats from '../assets/substats.png'
 import vscodeMathToImage from '../assets/vscode-math-to-image.png'
 
 export interface ProjectProps {
   name: string
   link: string
-  description: string
-  backgroundImage: string
+  slug: string
+  bimg: string
 }
 
 export const projects: ProjectProps[] = [
   {
     name: 'Substats',
     link: 'https://github.com/spencerwooo/substats',
-    description: 'spencerwooo/substats',
-    backgroundImage: substats,
+    slug: 'spencerwooo/substats',
+    bimg: substats,
   },
   {
     name: 'onedrive-cf-index',
     link: 'https://github.com/spencerwooo/onedrive-cf-index',
-    description: 'spencerwooo/onedrive-cf-index',
-    backgroundImage: onedriveCfIndex,
+    slug: 'spencerwooo/onedrive-cf-index',
+    bimg: onedriveCfIndex,
   },
   {
     name: 'dowww',
     link: 'https://github.com/spencerwooo/dowww',
-    description: 'spencerwooo/dowww',
-    backgroundImage: dowww,
+    slug: 'spencerwooo/dowww',
+    bimg: dowww,
   },
   {
     name: 'BIThesis',
     link: 'https://github.com/BITNP/BIThesis',
-    description: 'BITNP/BIThesis',
-    backgroundImage: bithesis,
+    slug: 'BITNP/BIThesis',
+    bimg: bithesis,
   },
   {
     name: 'vscode-math-to-image',
     link: 'https://github.com/TeamMeow/vscode-math-to-image',
-    description: 'TeamMeow/vscode-math-to-image',
-    backgroundImage: vscodeMathToImage,
+    slug: 'TeamMeow/vscode-math-to-image',
+    bimg: vscodeMathToImage,
   },
 ]
