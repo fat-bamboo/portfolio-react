49a06ffcd9ea459f25f44c081897b4b84dca0158
spencerwooo <spencer.woo@outlook.com>
2021-02-22 01:46:35 +0800

    remove splitbee deps and load via cf workers

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import splitbee from "@splitbee/web";
-
-splitbee.init({
-  token: 'IUM4YFFD0ZMR',
-})
 
 ReactDOM.render(
   <React.StrictMode>
