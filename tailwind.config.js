923a5dc595a54a497071b8c27f05831f80aeb1e1
spencerwooo <spencer.woo@outlook.com>
2021-08-09 21:48:29 +0100

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
@@ -8,8 +9,21 @@ module.exports = {
   ],
   darkMode: 'media',
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.green,
+      blue: colors.sky,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      pink: colors.pink
+    },
     extend: {
-      fontWeight: ['hover', 'focus'],
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
